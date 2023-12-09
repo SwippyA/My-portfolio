4f8abae11531606e6300afc297358d0184ee0fea
SwippyA <shubhnkarswain16@gmail.com>
2023-12-09 21:33:12 +0530

    ghg

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {TypeAnimation} from 'react-type-animation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-    <h1>shubhankar here</h1>
-   </>
+   <div>
+     <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Wed Developer',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Software engg',
+        1000,
+        'data minning',
+        1000,
+        'App developer',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+   </div>
   )
 }
 
