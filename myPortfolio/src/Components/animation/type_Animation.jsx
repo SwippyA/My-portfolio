2fcca395df5570e65fa2c9c866b88de334ebf233
SwippyA <shubhnkarswain16@gmail.com>
2023-12-09 21:54:22 +0530

    animation get in

---
+++ myPortfolio/src/Components/animation/type_Animation.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {TypeAnimation} from 'react-type-animation'
+function type_Animation() {
+  return (
+    <div className='text-yellow-400  w-1/2 transition-shadow '>
+    <TypeAnimation 
+     sequence={[
+       // Same substring at the start will only be typed out once, initially
+       'Wed Developer',
+       1000, // wait 1s before replacing "Mice" with "Hamsters"
+       'Software engg',
+       1000,
+       'data minning',
+       1000,
+       'App developer',
+       1000
+     ]}
+     wrapper="span"
+     speed={50}
+     style={{ 
+         
+         fontSize: '5em',
+         display: 'inline-block', 
+         fontWeight:'bold',
+         text:"yellow",
+         textShadow:"3px  4px 6px black"
+         
+     }}
+     repeat={Infinity}
+   />
+  </div>
+  )
+}
+
+export default type_Animation
