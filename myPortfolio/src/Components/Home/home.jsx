30ddc026f8b3b83c8536489326d1b0246f6a21e7
SwippyA <shubhnkarswain16@gmail.com>
2024-01-07 16:09:30 +0530

    home animation done

--- myPortfolio/src/Components/Home/home.jsx
+++ myPortfolio/src/Components/Home/home.jsx
@@ -1,20 +1,56 @@
 import React from "react";
 import Type_Animation from "../animation/type_Animation";
-import Head from "../Head/Head";
-import Footer from "../Footer/Footer";
-import { NavLink } from "react-router-dom";
+import {motion, spring} from "framer-motion"
+ import { NavLink } from "react-router-dom";
 function Home() {
   return (
     <>
       <div>
         <div className=" flex bg-yellow-200 w-full h-full items-center justify-between p-11">
           <div className="flex flex-col p-4 gap-2">
-            <div className="text-6xl">HI,I`M A FREELANCER </div>
+            <motion.div className="text-6xl"
+            animate={{
+              x:0,
+              rotate:0,
+              backgroundColor:"rgb(254 240 138)"
+              
+                    }}
+            initial={{
+              x:1000,
+              rotate:360,
+              backgroundColor:"blue"
+              
+
+            
+
+            }}
+            transition={{
+              type:"spring",
+              stiffness:30
+            }}
+            
+            
+            >HI,I`M A FREELANCER </motion.div>
             <div>
               <Type_Animation />
             </div>
             <div className="text-2xl font-semibold">based on Pune,India.</div>
-            <div className="flex gap-7 m-3 justify-start">
+            <motion.div 
+            className="flex gap-7 m-3 justify-start"
+            animate={{
+              y:0,
+              opacity:1
+            }}
+            initial={{
+              y:1000,
+              opacity:0
+            }}
+            transition={{
+              type: "spring",
+               stiffness: 100
+            }}
+            
+            >
               <NavLink
                 to="/project"
                 className="px-6 py-4 hover:bg-slate-800 duration-1000 bg-black text-white font-bold"
@@ -45,9 +81,25 @@ function Home() {
                   />
                 </svg>
               </a>
-            </div>
+            </motion.div>
           </div>
-          <div className=" mt-7  h-1/3  z-0">
+          <motion.div
+           className=" mt-7  h-1/3  z-0"
+           animate={{
+              scale:1,
+              opacity:1
+           }}
+           initial={{
+            scale:0.1,
+            opacity:0.1
+           }}
+           transition={{
+            duration:1
+            
+           }}
+           
+           
+           >
             <div className="w-11/12 h-11/12 z-0 bg-white p-7 box-border overflow-hidden rounded-full ">
               <img
                 className="saturate-0 rounded-full transition-transform duration-1000 hover:saturate-100 ease-in-out transform-gpu hover:scale-110   "
@@ -55,7 +107,7 @@ function Home() {
                 alt="Profile Image"
               />
             </div>
-          </div>
+          </motion.div>
         </div>
         <div></div>
         <div className="bg-yellow-200 flex justify-center p-1">
